eab17b53553b308dfcf75a7224c69a34c75e076b
Ivor Wanders <ivor@iwanders.net>
2024-12-15 17:20:11 -0500

    Add more of demodulation pipeline.

--- record_playback.py
+++ record_playback.py
@@ -24,6 +24,8 @@ from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 from gnuradio import gr, blocks
+from gnuradio.qtgui import Range, RangeWidget
+from PyQt5 import QtCore
 import sip
 
 
@@ -64,14 +66,22 @@ class record_playback(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
+        self.squelch_threshold_db = squelch_threshold_db = -40
+        self.squelch_alpha = squelch_alpha = 1.0
         self.samp_rate = samp_rate = 2048000
-        self.fsk_deviation = fsk_deviation = 20e3
+        self.fsk_deviation_hz = fsk_deviation_hz = 20e3
         self.bandpass_filter_width = bandpass_filter_width = 60e3
 
         ##################################################
         # Blocks
         ##################################################
 
+        self._squelch_threshold_db_range = Range(-100, 100, 1, -40, 200)
+        self._squelch_threshold_db_win = RangeWidget(self._squelch_threshold_db_range, self.set_squelch_threshold_db, "'squelch_threshold_db'", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._squelch_threshold_db_win)
+        self._squelch_alpha_range = Range(0, 1.0, 0.1, 1.0, 200)
+        self._squelch_alpha_win = RangeWidget(self._squelch_alpha_range, self.set_squelch_alpha, "'squelch_alpha'", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._squelch_alpha_win)
         self.qtgui_sink_x_1 = qtgui.sink_c(
             32768, #fftsize
             window.WIN_BLACKMAN_hARRIS, #wintype
@@ -108,16 +118,18 @@ class record_playback(gr.top_block, Qt.QWidget):
         self.qtgui_sink_x_0.enable_rf_freq(False)
 
         self.top_layout.addWidget(self._qtgui_sink_x_0_win)
-        self.freq_xlating_fir_filter_xxx_0 = filter.freq_xlating_fir_filter_ccc(1, firdes.complex_band_pass(1.0, samp_rate, fsk_deviation/2 - bandpass_filter_width, fsk_deviation/2 + bandpass_filter_width, bandpass_filter_width), (434100000 + 0.07e6), samp_rate)
+        self.freq_xlating_fir_filter_xxx_0 = filter.freq_xlating_fir_filter_ccc(1, firdes.complex_band_pass(1.0, samp_rate, fsk_deviation_hz/2 - bandpass_filter_width, fsk_deviation_hz/2 + bandpass_filter_width, bandpass_filter_width), (434100000 + 0.07e6), samp_rate)
         self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_gr_complex*1, samp_rate, True, 0 if "time" == "auto" else max( int(float(1) * samp_rate) if "time" == "time" else int(1), 1) )
         self.blocks_file_meta_source_0 = blocks.file_meta_source('/tmp/capture_query.grcbin', True, False, '/tmp/capture_query.grcbin')
+        self.analog_simple_squelch_cc_1 = analog.simple_squelch_cc(squelch_threshold_db, squelch_alpha)
         self.analog_agc_xx_1 = analog.agc_cc((1e-6), 1.0, 1.0, 65536)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_agc_xx_1, 0), (self.qtgui_sink_x_1, 0))
+        self.connect((self.analog_agc_xx_1, 0), (self.analog_simple_squelch_cc_1, 0))
+        self.connect((self.analog_simple_squelch_cc_1, 0), (self.qtgui_sink_x_1, 0))
         self.connect((self.blocks_file_meta_source_0, 0), (self.blocks_throttle2_0, 0))
         self.connect((self.blocks_throttle2_0, 0), (self.freq_xlating_fir_filter_xxx_0, 0))
         self.connect((self.blocks_throttle2_0, 0), (self.qtgui_sink_x_0, 0))
@@ -132,29 +144,43 @@ class record_playback(gr.top_block, Qt.QWidget):
 
         event.accept()
 
+    def get_squelch_threshold_db(self):
+        return self.squelch_threshold_db
+
+    def set_squelch_threshold_db(self, squelch_threshold_db):
+        self.squelch_threshold_db = squelch_threshold_db
+        self.analog_simple_squelch_cc_1.set_threshold(self.squelch_threshold_db)
+
+    def get_squelch_alpha(self):
+        return self.squelch_alpha
+
+    def set_squelch_alpha(self, squelch_alpha):
+        self.squelch_alpha = squelch_alpha
+        self.analog_simple_squelch_cc_1.set_alpha(self.squelch_alpha)
+
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
-        self.freq_xlating_fir_filter_xxx_0.set_taps(firdes.complex_band_pass(1.0, self.samp_rate, self.fsk_deviation/2 - self.bandpass_filter_width, self.fsk_deviation/2 + self.bandpass_filter_width, self.bandpass_filter_width))
+        self.freq_xlating_fir_filter_xxx_0.set_taps(firdes.complex_band_pass(1.0, self.samp_rate, self.fsk_deviation_hz/2 - self.bandpass_filter_width, self.fsk_deviation_hz/2 + self.bandpass_filter_width, self.bandpass_filter_width))
         self.qtgui_sink_x_0.set_frequency_range(443920000, self.samp_rate)
         self.qtgui_sink_x_1.set_frequency_range(0, self.samp_rate)
 
-    def get_fsk_deviation(self):
-        return self.fsk_deviation
+    def get_fsk_deviation_hz(self):
+        return self.fsk_deviation_hz
 
-    def set_fsk_deviation(self, fsk_deviation):
-        self.fsk_deviation = fsk_deviation
-        self.freq_xlating_fir_filter_xxx_0.set_taps(firdes.complex_band_pass(1.0, self.samp_rate, self.fsk_deviation/2 - self.bandpass_filter_width, self.fsk_deviation/2 + self.bandpass_filter_width, self.bandpass_filter_width))
+    def set_fsk_deviation_hz(self, fsk_deviation_hz):
+        self.fsk_deviation_hz = fsk_deviation_hz
+        self.freq_xlating_fir_filter_xxx_0.set_taps(firdes.complex_band_pass(1.0, self.samp_rate, self.fsk_deviation_hz/2 - self.bandpass_filter_width, self.fsk_deviation_hz/2 + self.bandpass_filter_width, self.bandpass_filter_width))
 
     def get_bandpass_filter_width(self):
         return self.bandpass_filter_width
 
     def set_bandpass_filter_width(self, bandpass_filter_width):
         self.bandpass_filter_width = bandpass_filter_width
-        self.freq_xlating_fir_filter_xxx_0.set_taps(firdes.complex_band_pass(1.0, self.samp_rate, self.fsk_deviation/2 - self.bandpass_filter_width, self.fsk_deviation/2 + self.bandpass_filter_width, self.bandpass_filter_width))
+        self.freq_xlating_fir_filter_xxx_0.set_taps(firdes.complex_band_pass(1.0, self.samp_rate, self.fsk_deviation_hz/2 - self.bandpass_filter_width, self.fsk_deviation_hz/2 + self.bandpass_filter_width, self.bandpass_filter_width))
 
 
 
