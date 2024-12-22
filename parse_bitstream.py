6eb5e0bc847d0ee3e4e754fa9949d15f221106e4
Ivor Wanders <ivor@iwanders.net>
2024-12-22 07:38:05 -0500

    Parsing packets.

--- parse_bitstream.py
+++ parse_bitstream.py
@@ -5,19 +5,44 @@ import socketserver
 def hexdump(z):
     return " ".join(f"{i:0>2x}" for i in z)
 
-class A7139BitstreamParser(socketserver.DatagramRequestHandler):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.buffer = []
+class A7139BitstreamParser:
+    def __init__(self):
+        self.buffer = bytearray([])
+        
+    def feed(self, data):
+        # If data is all ones, it's probably boring.
+        self.buffer += data
+        print(len(self.buffer))
+        while True:
+            # print(self.buffer)
+            # Try to find a preamble;
+            preamble = self.buffer.find(bytes([0, 1, 0, 1, 0, 1]))
+            # from the preamble, walk right until we encounter 'n' ones.
+            all_high = self.buffer.find(bytes([1] * 20), preamble + 1)
+            # print(f"p at {preamble}  high at {all_high}")
+            if preamble != -1  and all_high != -1:
+                packet = self.buffer[preamble:all_high+10]
+                self.buffer = self.buffer[all_high + 1:]
+                # print(f"Found packet at {packet}  {hexdump(packet)}")
+                print(f"{len(packet)}  {hexdump(packet)}")
+                continue
 
-    def handle(self):
-        data = self.rfile.read()
-        print(f"{self.client_address[0]}: {len(data)}: {hexdump(data)}");
-
+            # print(f"Stripping buffer and continuing")
+            self.buffer = self.buffer.lstrip(bytes([1]))
+            break
+        print(len(self.buffer))
 
 if __name__ == '__main__':
     listen_addr = ('0.0.0.0', 2003)
 
-    server = socketserver.UDPServer(listen_addr, A7139BitstreamParser)
+    parser = A7139BitstreamParser()
+
+    class GnuRadioTCPHandler(socketserver.DatagramRequestHandler):
+        def handle(self):
+            data = self.rfile.read()
+            # print(f"{self.client_address[0]}: {len(data)}: {hexdump(data)}");
+            parser.feed(data)
+
+    server = socketserver.UDPServer(listen_addr, GnuRadioTCPHandler)
     server.allow_reuse_address = True 
     server.serve_forever()
